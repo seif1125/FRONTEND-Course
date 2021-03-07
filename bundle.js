899505e19683d7251f1b5918bce93516d6788270
seif1125 <57404468+seif1125@users.noreply.github.com>
2021-03-07 19:27:23 +0200

    retreive products to shop page, implments product card

--- bundle.js
+++ bundle.js
@@ -98,17 +98,45 @@ module.exports={
 }
 },{}],2:[function(require,module,exports){
 const data = require('./products.json');
-const name=window.location.href.substr(window.location.href.indexOf('?')+1,window.location.href.length);
-document.getElementById('user').innerText=name;
- document.getElementsByClassName("radios")[0].addEventListener("click",
- ()=>{
-     document.getElementById("sectitle").innerText=document.getElementsByClassName("radios")[0].value;
- }
- )
-  document.getElementsByClassName("radios")[1].addEventListener("click",
- ()=>{
-     document.getElementById("sectitle").innerText=document.getElementsByClassName("radios")[1].value;
- }
- )
+
+let productDom=document.querySelector(".products-container");
+console.log(productDom)
+OrganizeDataFromJson(data);
+
+function OrganizeDataFromJson(){
+const playstationArr=data.playstation;
+const gamesArr=data.games;
+const allCategeories=playstationArr.concat(gamesArr)
+console.log(allCategeories)
+return showUiProducts(allCategeories,playstationArr,gamesArr);
+}
+function showUiProducts(allCat,playstationArr,gamesArr){
+ 
+  let g= allCat.map(item=>{
+
+        return `<div class="products-card">
+                     <img class="product-card-img"
+                        src=${item.photoalbumurl[0]}
+                         />
+                    <h3 class="product-card-name">${item.name}</h3>
+                    <p class="product-card-description">${item.description.substr(0,30)}</p>
+                    <span class="product-card-oldprice"><del>${item.price.originalprice+" LE"}</del></span>
+                    <span class="product-card-discount">${"-"+item.price.discount}</span>
+                    <i class="fas fa-heart like"></i>
+                    <br>
+                    <hr>
+                    <span class="product-card-finalprice">${item.price.finalprice+" LE"}</span>
+                    <button class="product-card-add" title="add to cart" value="add to cart">add to cart</button>
+                </div>`
+    })
+
+    g.forEach(element => {
+    productDom.innerHTML+=element;
+    console.log(element)      
+    });
+  
+    
+
+}
 
 },{"./products.json":1}]},{},[2]);
